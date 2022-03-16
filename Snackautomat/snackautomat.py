08a531b1d13025e762b8afa6a48dd098e65b0d68
Niklas <niklas-holl@gmx.de>
2022-03-16 21:35:19 +0100

    Snack machine works, pylint needs to be cleared

--- Snackautomat/snackautomat.py
+++ Snackautomat/snackautomat.py
@@ -9,39 +9,11 @@ def safe_input(value):
         exit()
 
 
-class Product:
-    def __init__(self, name, price, amount):
-        self.name = name
-        self.price = price
-        self.amount = amount
-
-    def buy(self):
-        if not self.amount == 0:
-            self.amount -= 1
-        else:
-            print("There ist no item left to buy!")
-
-
-class Keycard:
-
-    def __init__(self, name, balance):
-        self.balance = balance
-        self.username = name
-
-    def deposit(self, amount):
-        if not amount == 0.0:
-            self.balance += amount
-            print("Your new balance: " + str(self.balance) + "$")
-
-    def withdraw(self, amount):
-        if (self.balance - amount) < 0:
-            print("Not enough balance!")
-        else:
-            self.balance -= amount
-            print("Your new balance: " + str(self.balance) + "$")
-
-    def current_balance(self):
-        print("Current balance of your account:  " + str(self.balance) + "$")
+def check_input(value):
+    try:
+        return float(value)
+    except ValueError:
+        return 0.0
 
 
 class Snackautomat:
@@ -49,82 +21,97 @@ class Snackautomat:
     def __init__(self):
         self.products = []
         self.users = []
-        self.fill_machine()
+        self.check_products()
+        self.load_products()
+        self.check_userdata()
         self.load_userdata()
         self.current_account = 0
 
-    def fill_machine(self):
+    def check_products(self):
         if not os.path.isfile("products.json"):
-            with open("products.json", "x", encoding="utf-8") as file:
-                product = [{"name": "Snickers", "price": 1.2, "amount": 10, "id": 1},
-                           {"name": "Mars", "price": 1.2, "amount": 10, "id": 2},
-                           {"name": "Gummibärchen", "price": 2.5, "amount": 10, "id": 3},
-                           {"name": "Skittles", "price": 1.55, "amount": 10, "id": 4},
-                           {"name": "Bifi", "price": 2.3, "amount": 10, "id": 5},
-                           {"name": "Corny", "price": 2.2, "amount": 10, "id": 6},
-                           {"name": "Bounty", "price": 1.4, "amount": 10, "id": 7},
-                           {"name": "Knoppers", "price": 1.9, "amount": 10, "id": 8},
-                           {"name": "Mentos", "price": 1.35, "amount": 10, "id": 9},
-                           {"name": "Twix", "price": 1.2, "amount": 10, "id": 10}]
-                json.dump(product, file)
+            self.create_products()
+
+    def create_products(self):
+        with open("products.json", "x", encoding="utf-8") as file:
+            product = [{"name": "Snickers", "price": 1.2, "amount": 10, "id": 1},
+                       {"name": "Mars", "price": 1.2, "amount": 10, "id": 2},
+                       {"name": "Gummibärchen", "price": 2.5, "amount": 10, "id": 3},
+                       {"name": "Skittles", "price": 1.55, "amount": 10, "id": 4},
+                       {"name": "Bifi", "price": 2.3, "amount": 10, "id": 5},
+                       {"name": "Corny", "price": 2.2, "amount": 10, "id": 6},
+                       {"name": "Bounty", "price": 1.4, "amount": 10, "id": 7},
+                       {"name": "Knoppers", "price": 1.9, "amount": 10, "id": 8},
+                       {"name": "Mentos", "price": 1.35, "amount": 10, "id": 9},
+                       {"name": "Twix", "price": 1.2, "amount": 10, "id": 10}]
+            json.dump(product, file)
 
         with open("products.json", "r", encoding="utf-8") as file:
             self.products = json.load(file)
 
-    def load_userdata(self):
+    def load_products(self):
+        with open("products.json", "r", encoding="utf-8") as products:
+            self.products = json.load(products)
+
+    def update_products(self):
+        self.check_products()
+        with open("products.json", "w", encoding="utf-8") as products:
+            json.dump(self.products, products)
+
+    def check_userdata(self):
         if not os.path.isfile("userdata.json"):
-            with open("userdata.json", "x", encoding="utf-8") as userdata:
-                user = [{"name": "admin", "password": "admin", "balance": 10000000}]
-                json.dump(user, userdata, indent=4)
+            self.create_userdata()
+
+    def create_userdata(self):
+        with open("userdata.json", "x", encoding="utf-8") as userdata:
+            user = [{"name": "admin", "password": "admin", "balance": 10000000}]
+            json.dump(user, userdata, indent=4)
 
         with open("userdata.json", "r", encoding="utf-8") as userdata:
             self.users = json.load(userdata)
 
-    def create_account(self, name, password):
-        if not os.path.isfile("userdata.json"):
-            self.load_userdata()
-            return False
-        self.users.append({"name": name, "password": password, "balance": 0})
+    def load_userdata(self):
+        with open("userdata.json", "r", encoding="utf-8") as userdata:
+            self.users = json.load(userdata)
+
+    def update_user(self):
+        self.check_userdata()
         with open("userdata.json", "w", encoding="utf-8") as userdata:
             json.dump(self.users, userdata, indent=4)
-        card = Keycard(name, 0)
-        return card
+
+    def create_account(self, name, password):
+        self.check_userdata()
+        self.users.append({"name": name, "password": password, "balance": 0})
+        self.update_user()
+        return name
 
     def check_account(self, name):
-        if not os.path.isfile("userdata.json"):
-            self.load_userdata()
+        self.check_userdata()
         for user in self.users:
             if name == user["name"]:
                 return True
         return False
 
     def check_password(self, name, password):
-        if not os.path.isfile("userdata.json"):
-            self.load_userdata()
+        self.check_userdata()
         for user in self.users:
             if password == user["password"] and name == user["name"]:
                 return True
         return False
 
-    def print_users(self):
-        for user in self.users:
-            print(user)
-
     def login_account(self, name, password):
-        if not os.path.isfile("userdata.json"):
-            self.load_userdata()
+        self.check_userdata()
         for user in self.users:
             if name == user["name"] and password == user["password"]:
-                card = Keycard(name, user["balance"])
-                return card
+                return name
 
     def list_products(self):
         for product in self.products:
             if product["amount"] == 0:
-                print("ID | " + str(product["id"]) + " | " + str(product["name"]) +
+                print("| ID | " + str(product["id"]) + " | " + str(product["name"]) +
                       " | Price: " + str(product["price"]) + " | Out of Stock")
+
             else:
-                print("ID | " + str(product["id"]) + " | " + str(product["name"]) +
+                print("| ID | " + str(product["id"]) + " | " + str(product["name"]) +
                       " | Price: " + str(product["price"]) + " | Amount: " +
                       str(product["amount"]))
 
@@ -135,56 +122,121 @@ class Snackautomat:
             print("| Type in: [1] | login")
             print("|          [2] | create a new User")
             print("|          [3] | list products")
+            print("|          [4] | Exit")
             print("============================================================")
-            data = safe_input("Choose between the given options: ").split()
+
+            data = safe_input("| Choose between the given options: ").split()
 
             if not len(data) == 1:
                 print("Invalid! Please choose between the given Options 1-3")
                 continue
 
-            if len(data) == 1:
-                if not (data[0] == "1" or data[0] == "2" or data[0] == "3"):
-                    print("Please choose a number between 1 and 3")
+            if not (data[0] == "1" or data[0] == "2" or data[0] == "3" or
+                    data[0] == "4"):
+                print("Please choose a number between 1 and 4")
+                continue
+
+            if data[0] == "1":
+                data = safe_input("Put in your data [username] " +
+                                  " [password]: ").lower().split()
+                if not len(data) == 2:
+                    continue
+                if not self.check_password(data[0], data[1]):
+                    print("Your username or password are incorrect!")
+                    continue
+                self.current_account = self.login_account(data[0], data[1])
+                print("Account successfully logged in")
+
+            if data[0] == "2":
+                data = safe_input("Put in your wished data [username] " +
+                                  " [password]: ").lower().split()
+                if not len(data) == 2:
+                    continue
+                if self.check_account(data[0]):
+                    print("Account already exists! ")
+                    continue
+                self.current_account = self.create_account(data[0], data[1])
+                print("Account successfully created")
+
+            if data[0] == "3":
+                print("============================================================")
+                self.list_products()
+                continue
+
+            if data[0] == "4":
+                exit()
+
+            while True:
+                print("============================================================")
+                print("| Welcome: " + self.current_account)
+                print("| Type in: [1] | Deposit balance")
+                print("|          [2] | Show current balance")
+                print("|          [3] | List products")
+                print("|          [4] | Buy products by ID")
+                print("|          [5] | Logout")
+                print("============================================================")
+
+                data = safe_input("| Choose between the given options: ").split()
+
+                if not len(data) == 1:
+                    print("Invalid! Please choose between the given Options 1-3")
+                    continue
+
+                if not (data[0] == "1" or data[0] == "2" or data[0] == "3" or
+                        data[0] == "4" or data[0] == "5"):
                     continue
 
                 if data[0] == "1":
-                    data = safe_input("Put in your data [username] [password]: ").lower().split()
-                    if not len(data) == 2:
+                    data = safe_input("| Type in the amount you wish to deposit: ").split()
+                    if not len(data) == 1:
                         continue
-                    if not self.check_password(data[0], data[1]):
-                        print("Your username or password are incorrect!")
-                        continue
-                    self.current_account = self.login_account(data[0], data[1])
-                    print("Account successfully logged in")
+                    self.check_userdata()
+                    for user in self.users:
+                        if not user["name"] == self.current_account:
+                            continue
+                        user["balance"] = user["balance"] + check_input(data[0])
+                        print("| Your new balance: " + str(user["balance"]))
+                        self.update_user()
+                        break
 
                 if data[0] == "2":
-                    data = safe_input("Put in your wished data [username] [password]: ").lower().split()
-                    if not len(data) == 2:
-                        continue
-                    if self.check_account(data[0]):
-                        print("Account already exists! ")
-                        continue
-                    self.current_account = self.create_account(data[0], data[1])
-                    print("Account successfully created")
-                if data[0] == "3":
-                    print("============================================================")
-                    self.list_products()
-                    continue
-
-                while True:
-                    print("============================================================")
-                    print("| Welcome: " + str(self.current_account.username))
-                    print("| Type in: [1] | Deposit balance")
-                    print("|          [2] | Show current balance")
-                    print("|          [3] | List products")
-                    print("|          [4] | Buy products by ID")
-                    print("|          [5] | Logout")
-                    print("============================================================")
-                    data = safe_input("Input: ").lower().split()
                     if not len(data) == 1:
-                        if not (data[0] == "1" or data[0] == "2" or data[0] == "3"):
-                            print("Please choose a number between 1 and 3")
+                        continue
+                    self.check_userdata()
+                    for user in self.users:
+                        if user["name"] == self.current_account:
+                            print("| Your current balance: " + str(user["balance"]))
+
+                if data[0] == "3":
+                    if not len(data) == 1:
+                        continue
+                    self.list_products()
+
+                if data[0] == "4":
+                    data = safe_input("| Type in the product ID: ").split()
+                    if not len(data) == 1:
+                        continue
+                    _id = check_input(data[0])
+                    for product in self.products:
+
+                        if not check_input(product["id"]) == _id:
                             continue
+                        if not product["amount"] > 0:
+                            break
+                        product["amount"] = product["amount"] - 1
+                        for user in self.users:
+                            if not user["name"] == self.current_account:
+                                continue
+                            user["balance"] = user["balance"] - product["price"]
+                            print("| Your new balance: " + str(user["balance"]))
+                            self.update_user()
+                            self.update_products()
+                            break
+                        break
+
+                if data[0] == "5":
+                    self.current_account = 0
+                    break
 
 
 if __name__ == "__main__":
